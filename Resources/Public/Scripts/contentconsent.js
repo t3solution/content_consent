a8ec79b2685368cb3f976fcf31e49e69f9754ec1
Helmut Hackbarth <info@t3solution.de>
2022-11-12 11:18:21 +0100

    Update contentconsent.js

--- Resources/Public/Scripts/contentconsent.js
+++ Resources/Public/Scripts/contentconsent.js
@@ -10,12 +10,29 @@ function ajaxConsentCall(contentByUid, currentRecord, cookies)
 		 if (ajaxCall.readyState === 4) {
 			 if (ajaxCall.status === 200) {
 				 resultContainer.innerHTML = ajaxCall.responseText;
-				 // LazyLoad JS for EXT:t3sbootstrap - if you need a custom lazy load JS for your images, you have to put it here
-	 			if (resultContainer.dataset.lazy) {
-					new LazyLoad({
-						elements_selector: '.lazy',
-						threshold: 0
+
+				 // LazyLoad- & lightbox JS for EXT:t3sbootstrap - if you need a custom JS, you have to put it here
+				if (resultContainer.dataset.lazy) {
+						new LazyLoad({
+							elements_selector: '.lazy',
+							threshold: 0
+						});
+				}
+				if (resultContainer.dataset.lightbox == 1) {
+					// Baguette Box (lightbox)
+					baguetteBox.run('.gallery, .image-gallery', {
+						animation: 'fadeIn',
+						noScrollbars: true
 					});
+				}
+				if (resultContainer.dataset.lightbox == 2) {
+					// halkabox-{data.uid} (lightbox)
+					halkaBox.options({animation: 'fade',theme: 'dark'});
+					halkaBox.run('gallery-'+contentByUid);
+				}
+				if (resultContainer.dataset.lightbox == 3) {
+					// GLightbox (lightbox)
+					const lightbox = GLightbox({loop: true});
 				}
 			} else {
 				resultContainer.innerHTML = ajaxCall.statusText;
@@ -27,3 +44,4 @@ function ajaxConsentCall(contentByUid, currentRecord, cookies)
 	ajaxCall.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 	ajaxCall.send('contentByUid='+contentByUid+'&currentRecord='+currentRecord+'&cookies='+cookies);
 }
+	
