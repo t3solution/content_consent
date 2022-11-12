7d70a8b8b84cfa2d885fc722c591b87d6d36fcc7
Helmut Hackbarth <info@t3solution.de>
2022-11-12 10:39:46 +0100

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,13 +2,13 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Content Consent',
-    'description' => 'Provides a content consent plugin to load any content elements and custom plugins by ajax without jQuery! Best used with Bootstrap 5',
+    'description' => 'Provides a content consent plugin to load any content elements and custom plugins by ajax without jQuery! So you can include Google Maps or YouTube videos GDPR/DSGVO compliant. Best used with Bootstrap 5',
     'category' => 'plugin',
     'author' => 'Helmut Hackbarth',
     'author_email' => 'typo3@t3solution.de',
     'state' => 'stable',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-11.99.99',
