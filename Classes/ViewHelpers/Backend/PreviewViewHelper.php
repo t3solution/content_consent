2efa83dedf84c66a8d344b0e1ecd4fe175b5dff8
t3solution <info@t3solution.de>
2025-12-06 17:33:47 +0100

    Supports TYPO3 v14

--- Classes/ViewHelpers/Backend/PreviewViewHelper.php
+++ Classes/ViewHelpers/Backend/PreviewViewHelper.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 namespace T3S\ContentConsent\ViewHelpers\Backend;
 
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Core\Service\FlexFormService;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Backend\Routing\UriBuilder;
-use TYPO3\CMS\Extbase\Mvc\RequestInterface;
-use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 use TYPO3\CMS\Fluid\Core\Rendering\RenderingContext;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Core\Information\Typo3Version;
 
 /**
  * This file is part of the TYPO3 extension content_consent.
@@ -23,7 +23,7 @@ class PreviewViewHelper extends AbstractViewHelper
 
 	protected $escapeOutput = false;
 
-	public function initializeArguments()
+	public function initializeArguments(): void
 	{
 		$this->registerArgument('flexform', 'string', 'xml data', true);
 	}
@@ -32,30 +32,34 @@ class PreviewViewHelper extends AbstractViewHelper
 	{
 		$preview = 'Flexform is empty!';
 
-		if ($this->arguments['flexform'] != NULL) {
+		if (!empty($this->arguments['flexform'])) {
+			# TYPO3 major version
+			$majorVersion = GeneralUtility::makeInstance(Typo3Version::class)->getMajorVersion();
+			if ( $majorVersion == 13 ) {
+				$flexFormService = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Service\FlexFormService::class);
+				$flexFormDataArr = $flexFormService->convertFlexFormContentToArray($this->arguments['flexform']);
+			} else {
+				// v14
+				$flexFormTools = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools::class);
+				$flexFormDataArr = $flexFormTools->convertFlexFormContentToArray($this->arguments['flexform']);
+			}
 
-			$flexFormService = GeneralUtility::makeInstance(FlexFormService::class);
-			$flexFormDataArr = $flexFormService->convertFlexFormContentToArray($this->arguments['flexform']);
 			$contentByUid = (int) $flexFormDataArr['settings']['consent']['contentByUid'];
 			$contentConsent = BackendUtility::getRecord('tt_content', $contentByUid);
 			$pageConsent = BackendUtility::getRecord('pages', $contentConsent['pid']);
-
-			/** @var RenderingContext $renderingContext */
-			$renderingContext = $this->renderingContext;
-			/** @var RequestInterface $request */
-			$request = $renderingContext->getRequest();
 
 			$uriParameters = [
 				'edit' => [
 					'tt_content' => [$contentByUid => 'edit'],
 				],
-				'returnUrl' => $request->getAttribute('normalizedParams')->getRequestUrl(),
+				'returnUrl' => $this->getRequest()->getAttribute('normalizedParams')->getRequestUrl(),
 			];
 
 			/** @var UriBuilder $backendUriBuilder */
 			$backendUriBuilder = GeneralUtility::makeInstance(UriBuilder::class);
 			$editHref = $backendUriBuilder->buildUriFromRoute('record_edit', $uriParameters);
-			$editLink = '<a href="'.$editHref.'"><span class="t3js-icon icon icon-size-small icon-state-default icon-actions-document-open" data-identifier="actions-document-open" aria-hidden="true"><span class="icon-markup"><svg class="icon-color"><use xlink:href="/_assets/1ee1d3e909b58d32e30dcea666dd3224/Icons/T3Icons/sprites/actions.svg#actions-document-edit"></use></svg></span></span>  Edit tt_content_'.$contentByUid.'</a>';
+			$header = !empty($contentConsent['header']) ? $contentConsent['header'] : 'no header';
+			$editLink = '<a href="'.$editHref.'"><span class="t3js-icon icon icon-size-small icon-state-default icon-actions-document-open" data-identifier="actions-document-open" aria-hidden="true"><span class="icon-markup"><svg class="icon-color"><use xlink:href="/_assets/1ee1d3e909b58d32e30dcea666dd3224/Icons/T3Icons/sprites/actions.svg#actions-document-edit"></use></svg></span></span>  Edit tt_content_'.$contentByUid.' | '.$header.'</a>';
 
 			$preview = '<strong>Content Consent (loaded by Ajax):</strong></br></br>
 			<table class="table table-striped">
@@ -70,7 +74,7 @@ class PreviewViewHelper extends AbstractViewHelper
 					</tr>
 					<tr>
 						<th scope="row">Header:</th>
-						<td>'.$contentConsent['header'].'</td>
+						<td>'.$header.'</td>
 					</tr>
 					<tr>
 						<th scope="row">On page:</th>
@@ -84,6 +88,14 @@ class PreviewViewHelper extends AbstractViewHelper
 		}
 
 		return $preview;
+	}
+
+	private function getRequest(): ServerRequestInterface|null
+	{
+		if ($this->renderingContext->hasAttribute(ServerRequestInterface::class)) {
+			return $this->renderingContext->getAttribute(ServerRequestInterface::class);
+		}
+		return null;
 	}
 
 }
