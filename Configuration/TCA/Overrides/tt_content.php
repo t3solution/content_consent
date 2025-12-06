f1432aa5fb9e54563a7b7f27ce98f938641c2d3d
t3solution <info@t3solution.de>
2025-12-06 17:42:52 +0100

    Supports TYPO3 v14

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -3,27 +3,51 @@ declare(strict_types=1);
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Information\Typo3Version;
 
 defined('TYPO3') || die();
 
-$pluginSignature = ExtensionUtility::registerPlugin(
-	'content_consent',
-	'All',
-	'Content consent',
-	'contentconsenticon',
-	'plugins',
-	'Provides a content consent plugin to load any content elements and custom plugins by ajax without jQuery! So you can include Google Maps, YouTube- or Vimeo videos GDPR/DSGVO compliant. Best used with Bootstrap 5',
-);
+
+# TYPO3 major version
+$majorVersion = GeneralUtility::makeInstance(Typo3Version::class)->getMajorVersion();
+
+if ( $majorVersion == 13 ) {
+
+	$pluginSignature = ExtensionUtility::registerPlugin(
+		'content_consent',
+		'All',
+		'Content consent',
+		'contentconsenticon',
+		'plugins',
+		'Provides a content consent plugin to load any content elements and custom plugins by ajax without jQuery! So you can include Google Maps, YouTube- or Vimeo videos GDPR/DSGVO compliant. Best used with Bootstrap 5',
+	);
+
+	ExtensionManagementUtility::addPiFlexFormValue(
+		'*',
+		'FILE:EXT:content_consent/Configuration/FlexForms/Consent.xml',
+		$pluginSignature,
+	);
+
+} else {
+
+	// v14
+	$pluginSignature = ExtensionUtility::registerPlugin(
+		'content_consent',
+		'All',
+		'Content consent',
+		'contentconsenticon',
+		'plugins',
+		'Provides a content consent plugin to load any content elements and custom plugins by ajax without jQuery! So you can include Google Maps, YouTube- or Vimeo videos GDPR/DSGVO compliant. Best used with Bootstrap 5',
+		'FILE:EXT:content_consent/Configuration/FlexForms/Consent.xml'
+	);
+
+}
+
 
 ExtensionManagementUtility::addToAllTCAtypes(
 	'tt_content',
 	'--div--;Configuration,pi_flexform,',
 	$pluginSignature,
 	'after:subheader',
-);
-
-ExtensionManagementUtility::addPiFlexFormValue(
-	'*',
-	'FILE:EXT:content_consent/Configuration/FlexForms/Consent.xml',
-	$pluginSignature,
 );
